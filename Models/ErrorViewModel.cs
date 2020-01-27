4a60337b5a895d95fdcba8aafb309665e068aaea
Kerri Sierra-Scott <kerriss66@hotmail.com>
2020-01-27 11:35:39 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MilitaryJobsInventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
